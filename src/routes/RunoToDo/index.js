5094c801dba3ce03578c60a750c022e6c57d5d60
John Turnbull <johnadamturnbull@gmail.com>
2022-02-03 10:46:24 -0800

    Got passed Vite requires

--- src/routes/RunoToDo/index.js
+++ src/routes/RunoToDo/index.js
@@ -1,3 +1,7 @@
+// import { ObjectId } from 'mongodb';
+
+import clientPromise from '$lib/mongodb-client'
+
 export async function get(request) {
     //const completed = 'true';
     const completed = request.url.searchParams.get('completed') === 'true' ? true : false;
@@ -9,6 +13,21 @@ export async function get(request) {
     }
 };
 
+/*
+
+export async function post (request) {
+    const dbConnection = await clientPromise;
+    const db = dbConnection.db();
+    const collection = db.collection('ToDos');
+    const todo = JSON.parse(request.body);
+    const newTodo = await collection.insertOne(todo);
+    return {
+        status: 200,
+        body: {
+            newTodo
+        }
+    }
+}
 export async function post(request) {
 
 };
@@ -20,3 +39,5 @@ export async function put(request) {
 export async function del(request) {
 
 };
+
+*/
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,24 @@
-<h1>Runo ToDo practice try it in Code</h1>
+<script>
+import clientPromise from '$lib/mongodb-client.js'
+
+export async function post (request) {
+    const dbConnection = await clientPromise;
+    const db = dbConnection.db();
+    const collection = db.collection('ToDos');
+    const todo = JSON.parse(request.body);
+    const newTodo = await collection.insertOne(todo);
+    return {
+        status: 200,
+        body: {
+            newTodo
+        }
+    }
+}
+
+
+</script>
+
+
+
+
+<h1>Runo ToDo practice</h1>
